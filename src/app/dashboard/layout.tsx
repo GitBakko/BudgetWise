f0aa7b42a43ec12f2fc5ad65715fcd9bb1f1853f
Stefano Brunelli <bakko.posta@gmail.com>
2025-06-30 13:59:13 +0000

    il menù laterale, seppur gradevole, mi sembra un pochino "scarno", possi

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -80,7 +80,7 @@ export default function DashboardLayout({
                       }}
                     >
                       <Link href={item.href}>
-                        <item.icon className="h-4 w-4" />
+                        <item.icon />
                         <span>{item.label}</span>
                       </Link>
                     </SidebarMenuButton>
